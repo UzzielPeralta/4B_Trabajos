2a17da8e21089a6a5aa220d749fd674f3b02d51a
uzziel <20243ds035@utez.edu.mx>
2025-09-03 20:01:03 -0600

    cree el switch para los casos del menu

--- src/Main.java
+++ src/Main.java
@@ -18,3 +18,15 @@ public class Main {
             System.out.println("4. Salir");
             System.out.print("Elige una opción: ");
             opcion = scanner.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                    System.out.print("¿Cuánto depositaras?: ");
+                    double montoDeposito = scanner.nextDouble();
+                    deposito.hacerDeposito(miCuenta, montoDeposito);
+                    break;
+                case 2:
+                    System.out.print("¿Cuánto retiraras?: ");
+                    double montoRetiro = scanner.nextDouble();
+                    retiro.hacerRetiro(miCuenta, montoRetiro);
+                    break;
